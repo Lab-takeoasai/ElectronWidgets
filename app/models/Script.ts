f6a5256a6b4f0442eeeb14a6a094d1246400a16c
Takeo Asai <spitson+github@gmail.com>
2016-03-08 14:55:22 +0900

    .app version also works

--- app/models/Script.ts
+++ app/models/Script.ts
@@ -1,8 +1,6 @@
 /// <reference path="../../typings/main.d.ts"/>
 /// <reference path="./ScriptServer.ts"/>
 import Electron = require("electron");
-
-//import server = require("./ScriptServer");
 import angular = require("angular");
 import Server = require("../models/ScriptServer");
 
@@ -17,14 +15,11 @@ class Script {
   }
   exec() {
     let app = angular.module(this._name, []);
-    let script = new Server(this._name, "ls", 1000);
+    let script = new Server(this._name, "ps aux", 1000);
     app.controller("body", ["$scope", "$interval", ($scope, $interval) => {
-      $scope.stdout = script.exec();
-      /*$interval( () => {
-        let result = script.exec();
-        $scope.$apply(function () { // $apply is needed to be displayed ASAP (because of AJAX callback)
-          $scope.stdout = result;
-      }), this._interval; });*/
+      $interval(() => {
+        $scope.stdout = script.exec();
+      }, this._interval);
     }]);
   }
 
--- test/models/TestScriptServer.ts
+++ test/models/TestScriptServer.ts
@@ -9,13 +9,13 @@ describe("Script は, ", () => {
     });
 
     it("echo $((1+1))に2を返す", () => {
-        let result = new sc.ScriptServer("name", "echo $((1+1))", 1000).exec();
+        let result = new sc("name", "echo $((1+1))", 1000).exec();
         console.log(result);
         expect(result).toBe(2);
     });
 
     it("pwd に実行パスを返す", () => {
-        let result = new sc.ScriptServer("name", "pwd", 1000).exec();
+        let result = new sc("name", "pwd", 1000).exec();
         console.log(result);
         expect(result).toBe(2);
     });
